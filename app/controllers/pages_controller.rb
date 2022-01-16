1a3e042819f5dcbe0de5b62c2b771db87c2e263a
Fibrodyplasia <wdmcdonough@gmail.com>
2022-01-16 02:13:33 +0000

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+#above, the < indicates the PagesController is inheriting from ApplicationController
+  def home
+    #by convention, this expects a home.html.erb template, so no more code necessary
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #root is the default route. Here, application has an implicit '_controller' after it, 
+  #so it is really 'pages_controller#home'. The #hello tells it to find the 'home' method
+  #(also called an action in this context) that is defined inside pages_controller.
+  root 'pages#home'
+end
