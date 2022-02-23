5d478806daeeb511e685ef996b26583785ed4f28
William <wdmcdonough@gmail.com>
2022-02-23 01:03:10 +0000

    add one-to-many assoc

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  belongs_to :user
   # make sure the following fields are present
   # for an article to be saved
   # google ActiveRecord validations for more
