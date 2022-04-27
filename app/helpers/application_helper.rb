876c9ee90066e0e8714ad639fd02245dd285a810
William <wdmcdonough@gmail.com>
2022-04-27 04:10:15 +0000

    add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper # rubocop:todo Style/Documentation
+
+  def gravatar_for(user, options = { size: 80 })
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow")
+  end
+
 end
