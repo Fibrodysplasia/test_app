cf94aa1b3efe75b7d06b4e81431e7f039d725267
William <wdmcdonough@gmail.com>
2022-05-05 23:52:33 +0000

    negative integration test

---
+++ test/integration/create_category_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class CreateCategoryTest < ActionDispatch::IntegrationTest
+  test "get new category form and create category" do
+    get "/categories/new"
+    assert_response :success
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: { category: { name: "TestCat" } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "TestCat", response.body
+  end
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " " } }
+    end
+    # I will create a shared errors partial file and test for them here at some point
+  end
+end
